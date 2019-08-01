f28fcb823ef9976da71ec6f6e3cec53618298638
B&B SYMPHONY LLC <ahmed.benamrouche@gmail.com>
2019-08-01 14:25:28 -0700

    Tortoise to SVN

--- controlRoom_batch_server/server/app/components/controlroom/alert/alert.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	moduleId: module.id,
-  	selector: 'alert',
-  	templateUrl: 'alert.component.html',
-})
-export class AlertComponent  {  }
